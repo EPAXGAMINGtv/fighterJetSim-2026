0b10ad1844a3a7135d3cc26d4968afc8cd598260
EPAXGAMING <sureyjan@gmail.com>
2024-12-02 19:45:18 +0100

    Projektdateien hinzufügen.

---
+++ fighterJetSim 2026/fighterJetSim 2026.cpp
@@ -0,0 +1,12 @@
+﻿// fighterJetSim 2026.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "fighterJetSim 2026.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
